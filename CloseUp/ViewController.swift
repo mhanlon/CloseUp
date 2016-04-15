16729d47aee80148508c6a5693bf7ec87d453af2
Matt Hanlon <mhanlon@gmail.com>
2016-04-15 12:26:21 -0700

    Initial Commit

---
+++ CloseUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloseUp
+//
+//  Created by Matthew Hanlon on 4/15/16.
+//  Copyright © 2016 Q.I. Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CloseUpTests/CloseUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  CloseUpTests.swift
+//  CloseUpTests
+//
+//  Created by Matthew Hanlon on 4/15/16.
+//  Copyright © 2016 Q.I. Software. All rights reserved.
+//
+
+import XCTest
+@testable import CloseUp
+
+class CloseUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
